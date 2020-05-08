91e9db1803c51a1d4c2626c1025da267a5956d9a
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-05-08 11:51:29 -0400

    Problems

--- LibraryIndex/Form1.cs
+++ LibraryIndex/Form1.cs
@@ -13,11 +13,23 @@ namespace LibraryIndex
 {
     public partial class Form1 : Form
     {
-        List<string> bookObjects = File.ReadAllLines("BooksFile.txt").ToList();
+        List<Book> bookObjects = new List<Book>();
+        List<string> bookTypes = File.ReadAllLines("BooksFile.txt").ToList();
+
 
         public Form1()
         {
             InitializeComponent();
+
+
+            for (int i = 0; i < bookTypes.Count; i += 2)
+            {
+                int bookNumber = Convert.ToInt32(bookTypes[i]);
+                string bookTitle = bookTypes[i + 1];
+
+                Book bk = new Book(bookNumber, bookTitle);
+                bookObjects.Add(bk);
+            }
         }
 
         private void findButton_Click(object sender, EventArgs e)
@@ -42,9 +54,9 @@ namespace LibraryIndex
 
         }
 
-        public Boolean LinearSearch(List<string> searchList, int searchValue)
+        public Boolean LinearSearch(List<Book> searchList, int searchValue)
         {
-            foreach (string s in searchList)
+            foreach (Book s in searchList)
             {
                 if (s == searchValue + "")
                 {
@@ -57,7 +69,7 @@ namespace LibraryIndex
         public Boolean BinarySearch(List<string> searchList, int searchValue)
         {
             int low = 0;
-            int high = searchList.Count - 1;
+            int high = bookTypes.Count - 1;
 
             while(high >= low)
             {
